3900deb110ae889dbb49af4990de3d39afa0bafa
ulala <ulala@gmail.com>
2023-06-01 07:36:02 -0500

    feat: :memo: Calculator
    Desarrollo de la calculadora, se cambia la interface
    
    by: Jhon Hernandez

--- api.php
+++ api.php
@@ -1,17 +1,32 @@
 <?php
-    header("Content-Type:application/json");
 
-    if (isset($_POST['submit'])) {
-        if (isset($_POST['boton'])) {
-          $seleccionados = $_POST['boton'];
-          echo "<h2>Botones seleccionados:</h2>";
-          echo "<ul>";
-          foreach ($seleccionados as $seleccionado) {
-            echo "<li>$seleccionado</li>";
-          }
-          echo "</ul>";
-        } else {
-          echo "<h2>No se ha seleccionado ningún botón.</h2>";
-        }
-    }
+    if (isset($_POST['calcular'])) {
+      $num1 = $_POST['num1'];
+      $num2 = $_POST['num2'];
+      $operator = $_POST['operador'];
+
+      switch ($operator) {
+          case 'suma':
+              $result = $num1 + $num2;
+              break;
+          case 'resta':
+              $result = $num1 - $num2;
+              break;
+          case 'multiplicacion':
+              $result = $num1 * $num2;
+              break;
+          case 'division':
+              if ($num2 != 0) {
+                  $result = $num1 / $num2;
+              } else {
+                  $result = "Error: verifica los numeros ingresados";
+              }
+              break;
+          default:
+              $result = "Error: Operador inválido";
+              break;
+      }
+
+      echo " El resultado de la " . $operator . " del numero " . $num1 . " y el numero " . $num2 . " es: " . $result;
+  }
 ?>
