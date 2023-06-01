8c861ed85b84a82b85bfd97f8bc2ae6fef305dda
ulala <ulala@gmail.com>
2023-06-01 08:41:03 -0500

    feat(Calculator): :memo: Calculator
    Validacion de inputs
    
    by: Jhon Hernandez

--- api.php
+++ api.php
@@ -1,32 +1,41 @@
 <?php
 
-    if (isset($_POST['calcular'])) {
-      $num1 = $_POST['num1'];
-      $num2 = $_POST['num2'];
-      $operator = $_POST['operador'];
+  header ("Content-Type:application/json");
 
-      switch ($operator) {
-          case 'suma':
-              $result = $num1 + $num2;
-              break;
-          case 'resta':
-              $result = $num1 - $num2;
-              break;
-          case 'multiplicacion':
-              $result = $num1 * $num2;
-              break;
-          case 'division':
-              if ($num2 != 0) {
-                  $result = $num1 / $num2;
-              } else {
-                  $result = "Error: verifica los numeros ingresados";
-              }
-              break;
-          default:
-              $result = "Error: Operador inválido";
-              break;
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operador = $_POST['operador'];
+
+    if(!is_numeric($num1) || !is_numeric($num2)){
+      echo "Error, no sea loco mi perro";
+    } else{
+
+      switch ($operador) {
+        case 'suma':
+            $result = $num1 + $num2;
+          break;
+        case 'resta':
+            $result = $num1 - $num2;
+          break;
+        case 'multiplicacion':
+            $result = $num1 * $num2;
+          break;
+        case 'potenciacion':
+            $result = pow($num1, $num2);
+          break;
+        case 'division':
+            if ($num2 != 0) {
+                $result = $num1 / $num2;
+            } else {
+                $result = "Error: verifica los numeros ingresados";
+            }
+          break;
+        default:
+            $result = "Error: Operador inválido";
+          break;
       }
 
-      echo " El resultado de la " . $operator . " del numero " . $num1 . " y el numero " . $num2 . " es: " . $result;
-  }
+      echo " El resultado de la " . $operador . " del numero " . $num1 . " y el numero " . $num2 . " es: " . $result;
+    }
+  
 ?>
